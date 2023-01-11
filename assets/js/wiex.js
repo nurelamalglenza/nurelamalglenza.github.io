b07a5c951dd0f34922b18fa5c848b5228d47ba7e
Nour El Amal Glenza <nourelamalglenza@gmail.com>
2023-01-11 11:33:48 +0100

    First commit
    First commit

---
+++ assets/js/wiex.js
@@ -0,0 +1,109 @@
+/*
+  [JS Index]
+  
+  ---
+  
+  Template Name: Wiex - Personal Portfolio Template
+  Author:  ex-nihilo
+  Version: 1.0
+*/
+
+
+/*
+  1. preloader
+  2. show elements
+    2.1. show fadeIn
+    2.2. show borders
+    2.3. show hero
+  3. the calls
+    3.1. call home
+    3.2. call page
+  4. highlight navigation
+  5. to top arrow animation
+    5.1. to top arrow scroll to top
+  6. facts counter
+  7. skills bar
+  8. forms
+    8.1. newsletter form
+	8.2. contact form
+  9. page transitions
+  10. modals
+    10.1. sign up modal
+      10.1.1. sign up modal additional CLOSER
+    10.2. contact modal
+      10.2.1. contact modal additional CLOSER
+  11. YouTube player
+  12. slick slider
+    12.1. slick fullscreen slideshow
+  13. swiper slider
+    13.1. swiper parallax slider
+*/
+
+
+$(function() {
+    "use strict";
+
+    $(window).on("load", function() {
+        // 1. preloader
+        $("#preloader").fadeOut(600);
+        $(".preloader-bg").delay(400).fadeOut(600);
+
+        // 2. show elements
+        // 2.1. show fadeIn
+
+        // 2.2. show borders
+
+        setTimeout(function() {
+            $(".border-bottom, #wave, .social-icons-wrapper, .the-bottom-credits, .social-icons-wrapper-mobile, .the-bottom-credits-mobile, .border-bottom-waveless").removeClass("bottom-position");
+        }, 800);
+        // 2.3. show hero
+        $(".hero-bg").addClass("hero-bg-show");
+    });
+	
+    // 3. the calls
+    // 3.1. call home
+
+    // 3.2. call pag
+	
+    // 4. highlight navigation
+
+    // 5.1. to top arrow scroll to top
+
+	
+    // 6. facts counter
+
+	
+    // 7. skills bar
+
+	
+    // 8. forms
+    // 8.1. newsletter form
+
+    // 8.2. contact form
+
+	
+	// 9. page transitions
+
+	
+    // 10. modals
+    // 10.1. sign up modal
+
+    // 10.1.1. sign up modal additional CLOSER
+
+    // 10.2. contact modal
+
+    // 10.2.1. contact modal additional CLOSER
+
+	// 11. YouTube player
+
+	
+	// 12. slick slider
+	// 12.1. slick fullscreen slideshow
+
+	
+	// 13. swiper slider
+    // 13.1. swiper parallax slider
+
+
+
+});
